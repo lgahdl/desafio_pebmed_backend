9cb4efb605839d858f386501b761dde5297442dc
luizgustavoah <luizgustavoahsc@gmail.com>
2021-05-23 23:00:08 -0300

    trying to untrack dist folder;

--- dist/testing/testAuthTokens.js
+++
@@ -1,6 +0,0 @@
-let authTokensObj = {};
-// Key => Token, Value => id_user
-// @ts-ignore
-authTokensObj['test_api_token'] = 1;
-module.exports = authTokensObj;
-//# sourceMappingURL=testAuthTokens.js.map
